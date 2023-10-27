f1ef3d677ce6d176b057f593fc1b4831e778572b
Bonifacio Juarez ceja <bonijuarezceja96@outlook.com>
2023-10-27 13:47:38 -0600

    add new images

--- src/app/components/drinks.tsx
+++ src/app/components/drinks.tsx
@@ -1,134 +1,119 @@
 import React, { FC } from "react";
-
+import Image from "next/image";
+import refres from "../images/refres.jpg"
+import malteada from "../images/malteada.jpg"
+import chan from "../images/chan.jpg"
+import jugo from "../images/jugo.png"
+import agua from "../images/agua.jpg"
 
 const Drinks:FC = () => {
     return (
         <>
         <section className="body-font">
-            <div className="container px-5 py-24 mx-auto" >
-                <div className="flex flex-col text-center w-full mb-20">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-primary">Bebidas</h1>
+            <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col">
+                    <div className="h-1 bg-base-200 rounded overflow-hidden">
+                        <div className="w-24 h-full bg-secondary-content"></div>
+                    </div>
+                    <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+                        <h1 className="sm:w-2/5 text-base-900 font-medium title-font text-2xl mb-2 sm:mb-0">Bebidas</h1>
+                        <p className="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0"></p>
+                    </div>
                 </div>
-                <div className="flex flex-wrap -m-4">
-                <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Refrescos</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 15</strong>
-                            </p>
+                <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={refres} alt="refresco" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Refrescos</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 15</strong>
+                        </p>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Pepsi</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">7UP</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Mansanita</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Jarritos</span>
                             </li>
                         </ul>
                     </div>
-                    </div>
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Chanpolas</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 35</strong>
-                            </p>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={malteada} alt="malteada" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Malteadas</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 30</strong>
+                        </p>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Fresa</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Vainilla</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Chocolate</span>
                             </li>
-                            <li className="flex items-center gap1">
+                        </ul>
+                    </div>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={chan} alt="chan" className="object-cover object-center h-full w-full"/>
+                        </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Chanpolas</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 35</strong>
+                        </p>
+                        <ul className="mt-6 space-y-2">
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Fresa</span>
+                            </li>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Vainilla</span>
+                            </li>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Chocolate</span>
+                            </li>
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Galleta</span>
                             </li>
                         </ul>
                     </div>
-                    </div>
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Malteada</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 30</strong>
-                            </p>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={jugo} alt="chan" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Jugos</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 15</strong>
+                        </p>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Fresa</span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Vainilla</span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Chocolate</span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700"></span>
-                            </li>
-                        </ul>
-                    </div>
-                    </div>
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Jugos</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 15</strong>
-                            </p>
-                        </div>
-                        <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">19 Hermanos</span>
                             </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700"></span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700"></span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700"></span>
-                            </li>
                         </ul>
                     </div>
-                    </div>
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Agua</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 20</strong>
-                            </p>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={agua} alt="chan" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Agua</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 20</strong>
+                        </p>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Great Value 1 L</span>
                             </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700"></span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700"></span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700"></span>
-                            </li>
                         </ul>
-                    </div>
                     </div>
                 </div>
             </div>
--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -1,144 +1,154 @@
 import React, { FC } from "react";
 import Image from "next/image";
-import taco from '../images/taco.jpg'
+import taco from "../images/taco.jpg"
+import torta from "../images/torta.jpg"
+import quesa from "../images/quesa.jpg"
+import burger from "../images/burger.jpg"
+import torta2 from "../images/torta2.jpg"
+import papas  from "../images/papas.jpg"
 
+
+
+/**
+ * The Products component is a React functional component that displays a menu of food items with their
+ * prices and descriptions.
+ * @returns The Products component is returning a section containing a menu of food items. Each food
+ * item is displayed with an image, name, price, and a list of options.
+ */
 const Products:FC = () => {
     return (
         <>
         <section className="body-font">
-            <div className="container px-5 py-24 mx-auto" >
-                <div className="flex flex-col text-center w-full mb-20">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-primary">Nuestro Menu</h1>
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col">
+                    <div className="h-1 bg-base-200 rounded overflow-hidden">
+                        <div className="w-24 h-full bg-secondary-content"></div>
+                    </div>
+                    <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+                        <h1 className="sm:w-2/5 text-base-900 font-medium title-font text-2xl mb-2 sm:mb-0">Nuestro Menu</h1>
+                        <p className="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0"></p>
+                    </div>
                 </div>
-                {/*Tacos*/}
-                <div className="flex flex-wrap -m-4">
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Tacos</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 12</strong>
-                            </p>
+                <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={taco} alt="taco" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Tacos</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 12</strong>
+                        </p>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Bisteck</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Chorizo</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Pastor</span>
                             </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Combinados</span>
-                            </li>
-                        </ul>
-                </div>
-                    </div>
-                    {/*Tortas*/}
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Tortas</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 40</strong>
-                            </p>
-                        </div>
-                        <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Bisteck</span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Chorizo</span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Pastor</span>
-                            </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Combinados</span>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Combinado</span>
                             </li>
                         </ul>
                     </div>
-                    </div>
-                    {/*Burritos y Quesadillas*/}
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Burritos y Quesadillas</h2>
-                            <p className="mt-2 sm:mt-4">
-                                <strong className="text-3xl font-bold  sm:text-4xl">$ 35 CH / $45 G</strong>
-                            </p>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={torta} alt="taco" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Tortas</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 40</strong>
+                        </p>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Bisteck</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Chorizo</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Pastor</span>
                             </li>
-                            <li className="flex items-center gap1">
-                                <span className="text-secondary-700">Combinados</span>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Combinado</span>
                             </li>
                         </ul>
                     </div>
-                    </div>
-                    {/*Hamburguesas*/}
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Hamburgesas</h2>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={quesa} alt="taco" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Burritos y Quesadillas</h2>
+                        <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-base sm:text-4xl">$ 35 CH / $ 45 G </strong>
+                        </p>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Bisteck</span>
+                            </li>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Chorizo</span>
+                            </li>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Pastor</span>
+                            </li>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Combinado</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={burger} alt="taco" className="object-cover object-center h-full w-full"/>
+                        </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Hamburgesas</h2>
+                        <ul className="mt-6 space-y-2">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Sencilla o Pastor .... $45</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Doble .... $55</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Con Papas + $15</span>
                             </li>
                         </ul>
                     </div>
-                    </div>
-                    {/*Tortas 2*/}
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Tortas</h2>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={torta2} alt="taco" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Tortas</h2>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Salchicha o Jamon .... $40</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Milanesa .... $40</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Cubana .... $ 45</span>
                             </li>
                         </ul>
                     </div>
-                    </div>
-                    {/*Otros*/}
-                    <div className="p-4 md:w-1/3">
-                    <div className="card w-96 bg-base-100 border-base-200 rounded-2xl border p-6 shadow-sm sm:px-8 lg:p-12">
-                        <div className="text-center">
-                            <h2 className="text-lg font-medium  ">Otros</h2>
+                    <div className="p-4 md:w-1/3  sm:mb-0 mb-6">
+                        <div className="rounded-lg h-64 overflow-hidden">
+                            <Image src={papas} alt="taco" className="object-cover object-center h-full w-full"/>
                         </div>
+                        <h2 className="text-xl font-medium title-font text-primary mt-5">Otros</h2>
                         <ul className="mt-6 space-y-2">
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Quesadilla Sencilla .... $20CH / $30G</span>
                             </li>
-                            <li className="flex items-center gap1">
+                            <li className="flex items-center gap-1">
                                 <span className="text-secondary-700">Orden Papas .... $35</span>
                             </li>
+                            <li className="flex items-center gap-1">
+                                <span className="text-secondary-700">Alambre ... $80 / $100</span>
+                            </li>
                         </ul>
-                    </div>
                     </div>
                 </div>
             </div>
